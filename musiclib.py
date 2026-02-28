b5870b4047679580f4f3a2bc13b33c2218501586
Emanijaz15 <eijaz7357@gmail.com>
2026-02-28 09:05:47 +0500

    Update musiclib.py

--- musiclib.py
+++ musiclib.py
@@ -3,7 +3,7 @@ music={
     "pal pal":"https://www.youtube.com/watch?v=8of5w7RgcTc&list=RD8of5w7RgcTc&start_radio=1",
     "afsos":"https://www.youtube.com/watch?v=2FhgKp_lfJQ&list=RD2FhgKp_lfJQ&start_radio=1",
     "finding her":"https://www.youtube.com/watch?v=3Cp2QTBZAFQ&list=RD3Cp2QTBZAFQ&start_radio=1",
-    "safar":"https://www.youtube.com/watch?v=LsUdoy7EH7M&list=RDLsUdoy7EH7M&start_radio=1",
+    "safar":"https://www.youtube.com/watch?v=LsUdoy7EH7M&list=RDLsUdoy7EH7M&start_radio=1",   
     "par channa":"https://www.youtube.com/watch?v=TrPvQvbp3Cg&list=RDTrPvQvbp3Cg&start_radio=1",
     "heer":"https://www.youtube.com/watch?v=5R4UChPzRBc&list=RD5R4UChPzRBc&start_radio=1", 
     "talwinder":"https://www.youtube.com/watch?v=IltsOcCj1Ak&list=RDIltsOcCj1Ak&start_radio=1",  
